015ac9614397c5114440184f0daf02be8f5b8f9c
udid24 <avantikanagrale04@gmail.com>
2023-02-19 15:57:47 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var a=document.getElementById("header-title");
+a.style.borderBottom='solid 2px black';
+var b=document.getElementsByClassName('title');
+console.log(b);
+b[0].style.fontWeight='bold';
+b[0].style.color='green';
+var c=document.getElementsByClassName('list-group-item');
+c[2].style.backgroundColor='green';
+for(var i=0;i<c.length;i++)
+c[i].style.fontWeight='bold';
