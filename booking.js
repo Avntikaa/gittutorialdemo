693ee4d6d7458237c430243ab2ec42bab7a490d0
udid24 <avantikanagrale04@gmail.com>
2023-02-21 23:18:22 +0530

    editt the list

--- booking.js
+++ booking.js
@@ -1,7 +1,6 @@
-const val = document.getElementById('btm');
-
+let val = document.getElementById('btm');
+var i=0;
 val.addEventListener('click', fun1);
-var i = 0;
 var newh3 = document.createElement('ul');
 newh3.id = 'ulele';
 function fun1(e) {
@@ -29,12 +28,17 @@ li.appendChild(l2);
 li.appendChild(l3);
 li.appendChild(l4);
 li.appendChild(l5);
+   // adding a delete button
+
 var newdeltebtn = document.createElement('button');
    newdeltebtn.className = 'btn btn-danger btn-sm float-right delete';
    newdeltebtn.appendChild(document.createTextNode('X'));
    li.appendChild(newdeltebtn);
-   // adding a delete button
-   
+   //adding a edit button
+   var neweditbtn=document.createElement('button');
+   neweditbtn.className='edit btn';
+      neweditbtn.appendChild(document.createTextNode('Edit'));
+   li.appendChild(neweditbtn);
    newh3.appendChild(li);
    var newdiv = document.querySelector('#last');
    newdiv.appendChild(newh3);
@@ -60,4 +64,28 @@ function removeitem(e) {
       lielement.removeChild(li);
       localStorage.removeItem(li.children[1].textContent);
    }
+}
+
+//edit item
+ul.addEventListener('click',edititem);
+function edititem(e){
+ e.preventDefault();
+   var lielement = ul.children[1];
+   if (e.target.classList.contains('edit')) {
+document.getElementById("fnames").value=e.target.parentElement.children[0].textContent;  
+document.getElementById("femail").value=e.target.parentElement.children[1].textContent;  
+document.getElementById("ftel").value=e.target.parentElement.children[2].textContent;  
+document.getElementById("ftime").value=e.target.parentElement.children[3].textContent;  
+document.getElementById("fdate").value=e.target.parentElement.children[4].textContent;  
+
+ 
+val.addEventListener('click',function(){
+   fun4(lielement,e);
+});
+   }
+}
+fun4=(lielement,e)=>{ 
+   lielement.removeChild(e.target.parentElement);
+      localStorage.removeItem(e.target.parentElement.children[1].textContent);
+
 }
