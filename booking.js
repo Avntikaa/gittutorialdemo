8b3a730cacc9e1a8708ea28b3244c842e947d7f9
udid24 <avantikanagrale04@gmail.com>
2023-02-21 10:58:05 +0530

    local storage for multiple user

--- booking.js
+++ booking.js
@@ -6,13 +6,25 @@ function fun1(e){
 e.preventDefault();
 // localStorage.setItem('name',names); 
              var names=document.getElementById("fnames").value;
-          var email=document.getElementById("femail").value;
+          var emailadd=document.getElementById("femail").value;
      var phone=document.getElementById("ftel").value;
      var time=document.getElementById("ftime").value;
           var date=document.getElementById("fdate").value;
-          let obj={name:names,emails:email,phone:phone,time:time,date:date};
+          let obj={name:names,email:emailadd,phone:phone,time:time,date:date};
           var newobj=JSON.stringify(obj);//tonconvert object into string as localstorage contain string
-           localStorage.setItem('objects',newobj);
-          var parseobj=JSON.parse(localStorage.getItem("objects"));//to convert string intonobject
-        }
+          localStorage.setItem(emailadd,newobj);
+   var parseval=JSON.parse(localStorage.getItem(emailadd));//to convert string intonobject
+   //create new div
+   var newh3=document.createElement('ul');
+   var li=document.createElement('li');
+   li.className="list-group-item";
+   li.appendChild(document.createTextNode(parseval.name));
+      li.appendChild(document.createTextNode(parseval.email));
+            li.appendChild(document.createTextNode(parseval.phone));
+   li.appendChild(document.createTextNode(parseval.date));
+   li.appendChild(document.createTextNode(parseval.time));
+newh3.appendChild(li);
+   var newdiv=document.querySelector('#start');
+   newdiv.appendChild(newh3);
+     }
         
