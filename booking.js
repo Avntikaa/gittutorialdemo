aded523d881edb666b20733b56c0d0574512f157
Avantika <99116667+Ucan2022@users.noreply.github.com>
2023-03-01 17:28:52 +0530

    axios update

--- booking.js
+++ booking.js
@@ -5,7 +5,7 @@ val.addEventListener('click', addData);
 var newh3 = document.createElement('ul');
 newh3.id = 'ulele';
 window.onload=()=>{
-   axios.get("https://crudcrud.com/api/3111722366624a389fdc5c8e189cafa4/Bookingapp")
+   axios.get("https://crudcrud.com/api/5a0f1dc983ac4b848d68c84e7b196e30/Bookingapp")
    .then((response)=>{
       console.log(response.data);
       for(var i=0;i<response.data.length;i++)
@@ -28,7 +28,6 @@ window.onload=()=>{
 //    //create new div
 //    DisplayData(parseval);
 // }
-
 function addData(e){
    e.preventDefault();
    var names = document.getElementById("fnames").value;
@@ -37,7 +36,7 @@ function addData(e){
    var time = document.getElementById("ftime").value;
    var date = document.getElementById("fdate").value;
    let obj = { name: names, email: emailadd, phone: phone, time: time, date: date };
-axios.post("https://crudcrud.com/api/3111722366624a389fdc5c8e189cafa4/Bookingapp",obj)
+axios.post("https://crudcrud.com/api/5a0f1dc983ac4b848d68c84e7b196e30/Bookingapp",obj)
 .then(res=>{
    console.log(res.data);
    DisplayData(res.data);
@@ -94,7 +93,7 @@ function removeitem(e) {
       let li = e.target.parentElement;
       lielement.removeChild(li);
    let id=e.target.parentElement.children[0].textContent
-   axios.delete(`https://crudcrud.com/api/3111722366624a389fdc5c8e189cafa4/Bookingapp/${id}`)
+   axios.delete(`https://crudcrud.com/api/5a0f1dc983ac4b848d68c84e7b196e30/Bookingapp/${id}`)
 .then(res=>{
    console.log(res);   
 })
@@ -103,24 +102,27 @@ function removeitem(e) {
 
 //edit item
 ul.addEventListener('click',edititem);
+
 function edititem(e){
  e.preventDefault();
-   var lielement = ul.children[1];
    if (e.target.classList.contains('edit')) {
-document.getElementById("fnames").value=e.target.parentElement.children[0].textContent;  
-document.getElementById("femail").value=e.target.parentElement.children[1].textContent;  
-document.getElementById("ftel").value=e.target.parentElement.children[2].textContent;  
-document.getElementById("ftime").value=e.target.parentElement.children[3].textContent;  
-document.getElementById("fdate").value=e.target.parentElement.children[4].textContent;  
-
- 
-val.addEventListener('click',function(){
-   fun4(lielement,e);
-});
-   }
+document.getElementById("fnames").value=e.target.parentElement.children[1].textContent;  
+document.getElementById("femail").value=e.target.parentElement.children[2].textContent;  
+document.getElementById("ftel").value=e.target.parentElement.children[3].textContent;  
+document.getElementById("ftime").value=e.target.parentElement.children[4].textContent;  
+document.getElementById("fdate").value=e.target.parentElement.children[5].textContent; 
+let btn=document.getElementById('btm');
+btn.addEventListener('click',function(){
+    var lielement = ul.children[1];
+      let li = e.target.parentElement;
+      lielement.removeChild(li);
+   let id=e.target.parentElement.children[0].textContent
+   axios.delete(`https://crudcrud.com/api/5a0f1dc983ac4b848d68c84e7b196e30/Bookingapp/${id}`)
+.then(res=>{
+   console.log(res);   
+})
+  .catch(err=>console.log(err));
+})
 }
-fun4=(lielement,e)=>{ 
-   lielement.removeChild(e.target.parentElement);
-      localStorage.removeItem(e.target.parentElement.children[1].textContent);
 
 }
